f0ab7301a786a4a6190d0c910bf1b6c652df3495
mikeasta <thenaitrise@gmail.com>
2021-11-21 20:55:09 +0300

    Game class decorated.

--- source/GameComponents/Game/Game.cpp
+++ source/GameComponents/Game/Game.cpp
@@ -11,63 +11,82 @@ void Game::stop() {
 
 void Game::start() {
     if (!game_is_started) {
-        game_goes = true;
+
+        // ############# GAME KEEPPING INITIALIZING #############
+        game_goes       = true;
         game_is_started = true;
+        // ######################################################
 
-        // Create "observer" for checking controller updates
-        ControllerObserver cont_obs;
 
-        // Create test file stream
+        // ############# TEST FILE STREAM #############
         std::ofstream out;
         out.open("/home/mikeasta/Programming/OOP-GAME/source/GameComponents/Game/output.txt");
+        // ############################################
 
+
+        // ############# RESPONSE ENGINE #############
         auto response_lib = Response().getResponseLib();
+        // ###########################################
+
+
+        // ############# FIELD, PLAYER & ENEMIES INIT. #############
         unsigned int field_rows = 5;
         unsigned int field_cols = 10;
-        auto generator = FieldGenerator();
-        auto cells = generator.generateField(field_rows, field_cols);
-        auto new_field = Field(cells, field_rows, field_cols);
+        auto generator  = FieldGenerator();
+        auto cells      = generator.generateField(field_rows, field_cols);
+        auto new_field  = Field(cells, field_rows, field_cols);
         auto new_player = Player(10, 10, 20);
-
-        auto new_field_cli  = FieldCLI(new_field);
-        auto new_player_cli = PlayerCLI(new_player);
-        auto combat_drawer  = CombatCLI();
-        auto equipment_cli  = EquipmentCLI(new_player);
-
-        // Fill field
         FieldAggregate aggregator;
         aggregator.aggregate(new_field, new_player);
         auto player_controller = PlayerController(new_player, new_field);
+        auto enemy_center      = EnemyManageCenter(new_field);
+        // #########################################################
 
-        // Detect all enemies
-        auto enemy_center = EnemyManageCenter(new_field);
 
+        // ############# LOGGER SETUP #############
+        ControllerObserver cont_obs;
         cont_obs.addController(&player_controller);
         auto logger = Logger(cont_obs);
         logger.addLoggerUnit(std::cout, true, "cout");
         logger.addLoggerUnit(out, true, "output");
+        // ########################################
 
+
+        // ############# UI SETUP #############
         char command;
         system("clear");
+        auto new_field_cli  = FieldCLI(new_field);
+        auto new_player_cli = PlayerCLI(new_player);
+        auto combat_drawer  = CombatCLI();
+        auto equipment_cli  = EquipmentCLI(new_player);
+        // ####################################
 
+
+        // ############# GAME LOOP #############
         while (game_goes) {
 
-            // Enemy stepping
+            // ############# ENEMY STEPPING #############
             enemy_center.move_all();
+            // ##########################################
 
-            // Print
+
+            // ############# UI PRINTING #############
             new_field_cli.print();
             combat_drawer.print();
             new_player_cli.print();
             equipment_cli.print();
-
             logger.log();
+            // #######################################
 
-            // Player move
+
+            // ############# PLAYER MOVEMENT #############
             std::cin >> command;
             std::pair<std::string, Combat> response = player_controller.move(command);
-
             system("clear");
+            // ###########################################
+
+
+            // ############# CHECKING FOR PLAYER STATE #############
             if (response.first == response_lib["win"] ||
                 response.first == response_lib["draw"] ||
                 response.first == response_lib["loss"]) {
@@ -79,7 +98,11 @@ void Game::start() {
             if (response.first == response_lib["exit"] || response.first == response_lib["loss"]) {
                 stop();
             }
+            // ####################################################
+
         }
+        // #####################################
+
     }
 }
 
